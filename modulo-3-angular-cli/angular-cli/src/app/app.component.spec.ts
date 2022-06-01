3fe1e60ce843631bb391a6d71e11f987f0d9bc38
Anderson Silva <andssilva.23@gmail.com>
2022-06-01 07:22:27 -0400

    feat: :rocket: ng lint, ng test

--- modulo-3-angular-cli/angular-cli/src/app/app.component.spec.ts
+++ modulo-3-angular-cli/angular-cli/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-cli app is running!');
+    expect(compiled.querySelector('.content h1')?.textContent).toContain('Comandos e exemplos do Angular CLI');
   });
 });
