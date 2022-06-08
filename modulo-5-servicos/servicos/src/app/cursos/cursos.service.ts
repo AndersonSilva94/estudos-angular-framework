acaa29d5228833f798f4e732668543686153730d
Anderson Silva <andssilva.23@gmail.com>
2022-06-08 19:15:52 -0400

    feat: :rocket: Escopo de instâncias de serviços e módulos (singleton e várias instâncias)

--- modulo-5-servicos/servicos/src/app/cursos/cursos.service.ts
+++ modulo-5-servicos/servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService {
 
+  private cursos: string[] = ['Angular 2', 'React', 'Vue'];
+
+  constructor() {
+    console.log('CursosService')
+  }
+
   getCursos() {
-    return ['Angular 2', 'React', 'Vue'];
+    return this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this.cursos.push(curso);
   }
 }
