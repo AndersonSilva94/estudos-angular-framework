90fadacc10a89ca713491557543af57c76eef4f4
Anderson Silva <andssilva.23@gmail.com>
2022-06-12 10:42:51 -0400

    feat: :rocket: Rotas filhas

--- modulo-7-rotas/rotas/src/app/app.module.ts
+++ modulo-7-rotas/rotas/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AlunosModule } from './alunos/alunos.module';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 // import { CursosService } from './cursos/cursos.service';
@@ -23,6 +24,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
   ],
   // providers: [CursosService],
