d35a50456be1b8a4175b0459954565f03af69e90
Anderson Silva <andssilva.23@gmail.com>
2022-06-12 10:23:49 -0400

    feat: :rocket: Rotas: Criando um módulo de rotas de funcionalidade

--- modulo-7-rotas/rotas/src/app/cursos/cursos.module.ts
+++ modulo-7-rotas/rotas/src/app/cursos/cursos.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+// import { RouterModule } from '@angular/router';
 
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos.component';
+import { CursosRoutingModule } from './cursos.routing.module';
 import { CursosService } from './cursos.service';
 
 @NgModule({
@@ -18,7 +19,8 @@ import { CursosService } from './cursos.service';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    CursosRoutingModule
+    // RouterModule
   ]
 })
 
