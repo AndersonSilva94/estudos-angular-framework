840bc599d380df3dacec4aa4d28ba0ee2ffee991
Anderson Silva <andssilva.23@gmail.com>
2022-06-12 09:36:14 -0400

    feat: :rocket: Criando um módulo de funcionalidade

---
+++ modulo-7-rotas/rotas/src/app/cursos/cursos.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosComponent } from './cursos.component';
+import { CursosService } from './cursos.service';
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  providers: [
+    CursosService
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+
+export class CursosModule {}
