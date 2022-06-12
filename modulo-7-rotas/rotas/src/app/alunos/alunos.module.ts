6c01cf05985f945d1cc18f17692e46edcba35ca9
Anderson Silva <andssilva.23@gmail.com>
2022-06-12 14:48:44 -0400

    feat: :rocket: Rotas filhas: desenvolvendo as telas

--- modulo-7-rotas/rotas/src/app/alunos/alunos.module.ts
+++ modulo-7-rotas/rotas/src/app/alunos/alunos.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   declarations: [
@@ -13,11 +15,12 @@ import { AlunosRoutingModule } from './alunos.routing.module';
     AlunoDetalheComponent
   ],
   providers: [
-
+    AlunosService
   ],
   imports: [
     CommonModule,
-    AlunosRoutingModule
+    AlunosRoutingModule,
+    FormsModule
   ]
 })
 
