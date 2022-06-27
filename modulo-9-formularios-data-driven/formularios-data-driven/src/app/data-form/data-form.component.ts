eca8966ab37e824ea640390ac92e8b01ffd49ade
Anderson Silva <andssilva.23@gmail.com>
2022-06-26 22:41:35 -0400

    feat: :rocket: Formulários reativos: Autopopulando endereço com CEP (setValue e patchValue)

--- modulo-9-formularios-data-driven/formularios-data-driven/src/app/data-form/data-form.component.ts
+++ modulo-9-formularios-data-driven/formularios-data-driven/src/app/data-form/data-form.component.ts
@@ -10,7 +10,7 @@ import { map } from 'rxjs/operators'
 })
 export class DataFormComponent implements OnInit {
 
-  formulario: FormGroup = new FormGroup({});
+  formulario!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -73,6 +73,56 @@ export class DataFormComponent implements OnInit {
     if (campoEmail?.errors) {
       return campoEmail?.errors['email'] && campoEmail.touched;
     }
+  }
+
+  consultaCEP() {
+    let cep = this.formulario.get('endereco.cep')?.value
+
+    // cep somente com números
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "" && cep != null) {
+
+      //Expressão regular para validar o CEP.
+      let validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  populaDadosForm(dados: any) {
+
+    this.formulario.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+
+    // this.formulario.get('nome')?.setValue('Andy')
+  }
+
+  resetaDadosForm(){
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
   }
 
 }
