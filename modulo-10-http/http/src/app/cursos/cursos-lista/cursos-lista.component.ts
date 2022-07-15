7b626e4e4053d965d38b4d841fb297c02e185bce
Anderson Silva <andssilva.23@gmail.com>
2022-07-15 00:18:10 -0400

    feat: :rocket: Http GET: listar registros

--- modulo-10-http/http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ modulo-10-http/http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos!: Curso[];
+
+  unsubscribe: Subject<void> = new Subject();
+
+  constructor(
+    private service: CursosService,
+  ) { }
 
   ngOnInit(): void {
+    this.listarCursos();
+  }
+
+  listarCursos() {
+    this.service.list()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(dados => this.cursos = dados);
   }
 
 }
