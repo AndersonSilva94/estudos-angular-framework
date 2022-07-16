2cb69c0d2c13b7f3d5a47ee4b7e8dd270cd7883a
Anderson Silva <andssilva.23@gmail.com>
2022-07-16 19:34:33 -0400

    feat: :rocket: Http + RxJS: Unsubscribe automático

--- modulo-10-http/http/src/app/unsubscribe-rxjs/componentes/poc-unsub.component.ts
+++ modulo-10-http/http/src/app/unsubscribe-rxjs/componentes/poc-unsub.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EnviarValorService } from '../enviar-valor.service';
-import { Subscription } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-poc-unsub',
@@ -10,15 +10,24 @@ import { Subscription } from 'rxjs';
     </app-poc-base>
   `
 })
-export class PocUnsubComponent implements OnInit {
+export class PocUnsubComponent implements OnInit, OnDestroy {
 
   nome = 'Componente com unsubscribe';
   valor!: string;
 
+  sub!: Subscription;
+
   constructor(private service: EnviarValorService) { }
 
   ngOnInit() {
+    this.sub = this.service.getValor()
+      .pipe(tap(v => console.log(this.nome, v)))
+      .subscribe(novoValor => this.valor = novoValor)
+  }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    console.log(`${this.nome} foi destruído`)
   }
 
 }
