058a6f4eff9b7f6b3465e591fb4f71639e411087
Anderson Silva <andssilva.23@gmail.com>
2022-07-31 03:32:55 -0400

    feat: :rocket: Http: Criando operador RxJS customizado

--- modulo-10-http/http/src/app/upload-file/upload-file/upload-file.component.ts
+++ modulo-10-http/http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -38,17 +39,25 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if(this.files && this.files.size > 0) {
       this.service.upload(this.files, `${environment.BASE_URL}/upload`)
-        .subscribe((event: HttpEvent<Object>) => {
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress)
+            this.progress = progress
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('upload concluído'))
+        /* .subscribe((event: HttpEvent<Object>) => {
           // HttpEventType
-          console.log(event);
+          // console.log(event);
           if (event.type === HttpEventType.Response) {
             console.log('upload concluído');
           } else if(event.type === HttpEventType.UploadProgress) {
             const percentDone = Math.round((event.loaded * 100)/ Number(event.total))
-            console.log('Progresso', percentDone)
+            // console.log('Progresso', percentDone)
             this.progress = percentDone
           }
-        });
+        }); */
     }
   }
 
