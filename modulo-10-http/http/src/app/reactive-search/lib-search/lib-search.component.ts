76bc8b0921541aa59aa3730747539b5e878e9e76
Anderson Silva <andssilva.23@gmail.com>
2022-07-31 21:22:35 -0400

    feat: :rocket: Http: Passando Parâmetros na URL (HttpParams)

--- modulo-10-http/http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ modulo-10-http/http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,22 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-lib-search',
   templateUrl: './lib-search.component.html',
-  styleUrls: ['./lib-search.component.scss']
+  styleUrls: ['./lib-search.component.scss'],
 })
 export class LibSearchComponent implements OnInit {
-
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$!: Observable<any>;
+  total!: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSearch() {
-    console.log(this.queryField.value)
-  }
+    const fields = 'name,author,description,filename,sri,version';
+    let value = this.queryField.value;
 
+    if (value && (value = value.trim()) !== '') {
+
+      const params_ = {
+        search: value,
+        fields: fields
+      }
+
+      // dar preferência para essa forma de setar parâmetros do que setar manualmente
+      let params = new HttpParams();
+      params = params.set('search', value);
+      params = params.set('fields', fields)
+
+      this.results$ = this.http
+        .get(this.SEARCH_URL, {
+          params
+        })
+        .pipe(
+          tap((res: any) => (this.total = res.total)),
+          map((res) => res.results)
+        );
+    }
+  }
 }
